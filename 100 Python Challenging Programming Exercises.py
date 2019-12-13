a077e1f8add49f2e050f2641d0f0b8bbe8c16907
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-12-14 00:05:39 +0100

    Solved 35 problems

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -595,10 +595,21 @@ print(next(evenNumbers))
 # QUESTION 34
 # Write a program which can map() to make a list whose elements are square of elements in [1,2,3,4,5,6,7,8,9,10].
 
-double = map(lambda y: y**2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+square = map(lambda y: y**2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-print(double)
-print(next(double, default="Stop bro!"))
+print(square)
+print(next(square, default="Stop bro!"))
+
+# ####################################################
+# QUESTION 35
+# Write a program which can map() and filter() to make a list whose elements are square of
+# even number in [1,2,3,4,5,6,7,8,9,10].
+
+even_filtered = [even for even in filter(lambda u: u % 2 == 0, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])]
+
+squared_even = [square for square in map(lambda m: m**2, even_filtered)]
+
+
 
 
 
