e4f15ef085a1bd21fc4f5ca8c8021a1550779e0f
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2020-01-15 10:39:09 +0100

    Solved 41 problems

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -708,3 +708,77 @@ class Square(Shape):
 
 sq = Square(6)
 sq.area()
+
+
+####################################################
+# QUESTION 38
+# Pleas raise a RuntimeError exception.
+
+raise RuntimeError("something is fishy")
+
+
+####################################################
+# QUESTION 39
+# Write a function to compute 5/0 and use try/except to catch the exceptions.
+
+def divider():
+    return 5/0
+
+
+try:
+    divider()
+except ZeroDivisionError as e:
+    print(f"error occurred - {e}")
+else:
+    print("Division Successful")
+finally:
+    print("Testing division")
+
+
+####################################################
+# QUESTION 40
+# Define a custom exception class which takes a string message as attribute.
+
+class Xception(Exception):
+    """My custom exception class
+    Attributes:
+    message =  something went wrong
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
+####################################################
+# QUESTION 41
+# Assuming that we have some email addresses in the "username@companyname.com" format,
+# please write program to print the user name of a given email address.
+# Both user names and company names are composed of letters only.
+# Example: If the following email address is given as input to the program:
+# john@google.com
+# Then, the output of the program should be: john
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+import re
+
+
+class NameFinder:
+    def __init__(self):
+        self.email = input("Enter your email here : ")
+
+    def check_email(self):
+        self.email_regex = re.compile(pattern=r"([A-Za-z]+)(@)([A-Za-z]+)(\.com)")
+        if self.email_regex.match(self.email):
+            return True
+        else:
+            return False
+
+    def name_xtractor(self):
+        if self.check_email():
+            name = self.email_regex.search(self.email).group(1)
+            print(f"Name = {name}")
+        else:
+            print("Invalid Email")
+
+
+name = NameFinder()
+name.name_xtractor()
