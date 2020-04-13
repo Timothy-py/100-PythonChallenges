39a3003b990933dfcda45808cb926d3fa56eafc7
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-13 22:21:01 +0100

    solved 52 problems

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -984,3 +984,24 @@ def fibonacci(n):
 
 i = int(input("Enter an integer value here: "))
 print([fibonacci(_) for _ in range(i)])
+
+
+# ###########################################################
+# QUESTION 52
+# Please write a program using generator to print the even numbers between 0 and n
+# in comma separated form while n is input by console.
+# 	Example: If the following n is given as input to the program: 10
+# 	Then, the output of the program should be: 0,2,4,6,8,10
+
+def even_gen(n):
+    for num in range(0, n+1):
+        if num % 2 == 0:
+            yield num
+
+
+n = int(input("Enter an integer number here: "))
+values = []
+for _ in even_gen(n):
+    values.append(str(_))
+
+print(",".join(values))
