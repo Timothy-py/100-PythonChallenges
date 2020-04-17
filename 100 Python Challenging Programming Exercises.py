73575f6fcecf4fd39d2429c2af25b4d3cf4dd221
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-17 23:48:47 +0100

    solved 53 problems

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -1005,3 +1005,25 @@ for _ in even_gen(n):
     values.append(str(_))
 
 print(",".join(values))
+
+
+# ###########################################################
+# QUESTION 53
+# 	Please write a program using generator to print the numbers which can be divisible by 5 and 7
+# 	between 0 and n in comma separated form while n is input by console.
+#   Example: If the following n is given as input to the program: 100
+#   Then, the output of the program should be: 0,35,70
+
+def generator(n):
+
+    for _ in range(0, n+1):
+        if _ % 5 == 0 and _ % 7 == 0:
+            yield _
+
+
+n_input = int(input("Enter an integer number here : "))
+values = []
+for i in generator(n_input):
+    values.append(str(i))
+
+print(",".join(values))
