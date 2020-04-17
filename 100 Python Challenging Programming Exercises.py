be8951e0833d4eb5781343ebb9672d32d174be21
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-18 00:06:03 +0100

    solved 53 problems

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -1027,3 +1027,11 @@ for i in generator(n_input):
     values.append(str(i))
 
 print(",".join(values))
+
+
+# ###########################################################
+# QUESTION 54
+# Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+
+for num in [2, 4, 6, 8]:
+    assert num % 2 == 0, f"{num} is not an even number bro!"
