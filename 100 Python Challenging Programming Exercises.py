053c79d025b408712fae2149d291d672d50bf025
Timothy-py <adeyeyetimothy33@gmail.com>
2020-05-15 01:27:34 +0100

    solved 58 problems

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -1091,6 +1091,16 @@ bin_search()
 # QUESTION 57
 # Please generate a random float where the value is between 10 and 100 using Python math module.
 
-from random import random
+import random
 
-print(f"{random()*100:.5f}")
+print(f"{random.random()*100:.5f}")
+
+
+# ##########################################################
+# QUESTION 58
+# Please write a program to output a random even number between 0 and 10 inclusive using random module
+# and list comprehension.
+
+import random
+
+print(random.choice([i for i in range(0, 11) if i % 2 == 0]))
