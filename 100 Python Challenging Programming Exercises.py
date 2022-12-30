e43fe88f88a33f85747b9982a3e3155169f57346
Timothy-py <adeyeyetimothy33@gmail.com>
2022-12-30 23:41:45 +0100

    solved challenge

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -843,29 +843,6 @@ pyrabuilder()
 # Example: If the following n is given as input to the program: 5
 # Then, the output of the program should be: 3.55
 
-def formula(n):
-    result = 0
-    if n > 0:
-        list_n = [i for i in range(1, n+2)]
-
-        for num in list_n[:-1]:
-            result += num/list_n[list_n.index(num)+1]
-        print(f"{result:.2f}")
-    else:
-        print("Value must be greater than 0")
-
-# *******************OR*******************
-
-
-def formula(n):
-    result = 0
-    for i in range(1, n+1):
-        result += i/(i+1)
-    print(f"{result:.2f}")
-
-
-formula(5)
-
 
 # ###########################################################
 # QUESTION 48
