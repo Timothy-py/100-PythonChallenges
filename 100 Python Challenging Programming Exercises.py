ed8d3ea34698185716b28784ac5150995ae050f8
Timothy-py <adeyeyetimothy33@gmail.com>
2023-01-24 22:32:14 +0100

    solved

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -585,20 +585,6 @@ squared_even = [square for square in map(lambda m: m**2, even_filtered)]
 # QUESTION 36
 # Define a class named American which has a static method called printNationality.
 
-class American:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def printNationality():
-        print("I am an American")
-
-
-American.printNationality()  # or
-AM = American()
-AM.printNationality()
-
-
 # ####################################################
 # QUESTION 37
 # Define a class named American and its subclass NewYorker.
---
+++ Q36.py
@@ -0,0 +1,14 @@
+# Define a class named American which has a static method called printNationality.
+
+class American:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def printNationality():
+        print("I am an American")
+
+
+American.printNationality()  # or
+AM = American()
+AM.printNationality()
