7934936d3dcdd8a02d35668a28c76ae1987fab5a
Timothy-py <adeyeyetimothy33@gmail.com>
2023-01-25 23:23:10 +0100

    solved challenge

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -555,11 +555,6 @@ for i in range(100):
 # Write a program which can filter even numbers in a list by using filter function.
 # The list is: [1,2,3,4,5,6,7,8,9,10].
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-evenNumbers = filter(lambda x: x % 2 == 0, numbers)
-print(evenNumbers)
-print(next(evenNumbers))
-print(next(evenNumbers))
 
 # ###################################################
 # QUESTION 34
