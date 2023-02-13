831940037fdd5b97febcf49c34edc9468bac609a
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-13 23:39:46 +0100

    solved

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -502,33 +502,6 @@ print(f"First half = {tup1} \n Second half = {tup2}")
 # Create a function to print the nth term of a fibonacci sequence using recursion, and
 # also use memoization to cache the result.
 
-# define a dictionary to serve as a cache
-fib_cache = {}
-
-
-def fib(n):
-    # check if the value to be computed is in the dictionary
-    # if it is there, return the result instead
-    if n in fib_cache:
-        return fib_cache[n]
-
-    # do the fibonacci computation here
-    if n == 0:
-        value = 0
-    elif n == 1:
-        value = 1
-    elif n >= 2:
-        value = fib(n-1) + fib(n-2)
-
-    # store the result of any of the above computation in the cache
-    fib_cache[n] = value
-
-    return value
-
-
-for i in range(100):
-    print(i, ':', fib(i))
-
 
 # ##################################################
 # QUESTION 32
