dfc21e0b9da069995317ce094853057ce9e74cd9
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-23 22:37:16 +0100

    solved challenge 15

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -190,12 +190,6 @@ print(int(b) + int(b+b) + int(b+b+b) + int(b+b+b+b))
 # A program which uses list comprehension to square each odd number in a list.
 # The list is input by a sequence of comma-separated numbers.
 
-string = input('Enter a list of comma-separated numbers here : ')
-nums = string.split(',')
-int_num = [int(x) for x in nums]
-output = [x**2 for x in int_num if x % 2 != 0]
-print(output)
-
 # ##############################################
 # Question 16
 # Write a program that computes the net amount of a bank account based a transaction log from console input.
