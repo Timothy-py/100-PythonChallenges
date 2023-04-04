10aae26aa3c943cc42907dcf57cc4b9d076c562e
Timothy-py <adeyeyetimothy33@gmail.com>
2023-04-04 23:39:53 +0100

    solved challenge 12

--- 100 Python Challenging Programming Exercises.py
+++ 100 Python Challenging Programming Exercises.py
@@ -159,10 +159,6 @@ for num in bin_num:
 # Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of
 # the number is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
 
-for num in range(1000, 3001):
-    if num % 2 == 0:
-        print(num, end=',')
-
 # ##############################################
 # Question 13
 # A program that accepts a sentence and calculate the number of letters and digits.
